95a64f1d91f66cbd747065cc67dfa280d3fa7098
Dp2o <snufylupicus@gmail.com>
2025-06-05 09:51:39 +1200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,7 +4,9 @@ let RocketPower = 0;
 
 // Simulator variables
 const AirFriction = 0.05;
-
+const GridSpace = {
+  
+  
 // website variables
 let screen = "main";
 let screenlist = ["main", "PlayScreen"];
@@ -22,4 +24,29 @@ function ChangeScreen(ChangeScreen) {
     Debug.show("--screen showed due to error--");
     console.log("invalid screen change");
   }
+}
+
+function SimLoop() {
+  
+  requestAnimationFrame(SimLoop);
+}
+
+function moveGridDotsWithKeys() {
+  // Move grid dots
+  gridDots.forEach((dot) => {
+    dot.x -= AccelerationX;
+    dot.y -= AccelerationY;
+
+    // Wrap around the canvas boundaries
+    if (dot.x > canvas.width) dot.x = 0;
+    if (dot.x < 0) dot.x = canvas.width;
+    if (dot.y > canvas.height) dot.y = 0;
+    if (dot.y < 0) dot.y = canvas.height;
+  });
+
+  // Move enemies along with the map to make it look like the player is moving
+  enemies.forEach((enemy) => {
+    enemy.y -= AccelerationY; // Move down
+    enemy.x -= AccelerationX; // Move right
+  });
 }
